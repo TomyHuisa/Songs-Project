4643ef4138f853575dfcb315567f3491936906cf
Tomy Huisa <huisatomy2007@gmail.com>
2024-07-08 11:28:58 -0300

    Agregado Base.html y New_artist, Arreglo de errores, Falta Terminar los Links

--- music/artists.py
+++ music/artists.py
@@ -1,5 +1,16 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, render_template, request, url_for
 from . import db
+
+consulta = """
+
+            SELECT Name FROM artists
+
+           """
+
+res = con.execute(consulta, (id,))
+artist = res.fetchall()
+pagina = render_template('songs.html',
+                        artists=artist)
 
 bp = Blueprint('artist', __name__, url_prefix='/artist')
 @bp.route('/<int:id>')
@@ -23,3 +34,20 @@ def detalle(id):
                             artist=artist,
                             albums=albumlist)
     return pagina
+
+@bp.route('/new', methods=('GET', 'POST'))
+def nuevo():
+    if request.method == 'POST':
+        Name = request.form['Name']
+        
+        con = db.get_db()
+        consulta = """
+                    INSERT INTO artist(Name)
+                    VALUES (?);
+                   """
+        con.execute(consulta, (Name))
+        con.commit()
+        return redirect(url_for('artist.artist'))
+    else:
+        pagina = render_template('new_artist.html')
+        return pagina
